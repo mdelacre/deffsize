46477257c6d1bdf23c45141d0247ca43743b46fe
arcaldwell49 <arcaldwell49@gmail.com>
2021-04-29 10:42:12 -0400

    Added back old calculations
    The app now runs a little smoother and added app2 which uses the old cdf calculations

--- testing_script.R
+++ testing_script.R
@@ -1,16 +1,31 @@
 library(tidyr)
-test_c= cohen_curv(m1 = 0,
-                   m2 = 1,
-                   sd1 = 1,
-                   sd2 = 1,
-                   n1 = 15,
-                   n2 = 15,
-                   steps = 1000,
-                   corr = FALSE,
-                   var.equal = TRUE)
+test_c= cohen_curv(m1 = 4,
+                   m2 = 0,
+                   sd1 = 2,
+                   sd2 = 2,
+                   n1 = 20,
+                   n2 = 20,
+                   steps = 10000,
+                   corr = TRUE,
+                   var.equal = FALSE)
 
-p1 = gg_curv_t(test_c[[1]],
-           type = "c")
+#p1 = gg_curv_t(test_c[[1]],
+#           type = "c")
 
 p2 = gg_curv_t(test_c[[2]],
                type = "cd")
+
+plot1 <- plot_cohen_curv(
+  smd_label = "Hedges g",
+  m1 = 4,
+  m2 = 0,
+  sd1 = 2,
+  sd2 = 2,
+  n1 = 20,
+  n2 = 20,
+  cdf_dat = test_c[[2]],
+  corr = TRUE,
+  var.equal = FALSE,
+  ci_shades = c(.5, .90, .95, .99),
+  ci_line = .95
+)
